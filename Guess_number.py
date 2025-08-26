faa54c33d6202161965f70c577eebfadb15ea7ac
GianlucaVallini99 <gianlucavallini92@gmail.com>
2025-08-26 15:52:14 +0200

    Create Guess_number.py

---
+++ Guess_number.py
@@ -0,0 +1,57 @@
+import random
+
+
+def crea_numero():
+    numero = random.randint(1, 100)
+    return numero
+
+def difficolta():
+    while True:
+        print("Scegli la difficoltà: ")
+        Choice = input("FACILE o DIFFICILE? ")
+        if Choice == "FACILE":
+            tentativi = 10
+            break
+        elif Choice == "DIFFICILE":
+            tentativi = 5
+            break
+        else:
+            print("Scelta non valida ")
+    return tentativi
+
+def confronta(numero, vite):
+    i = 0
+    while vite > 0:
+        guess = int(input("Inserisci il numero: "))
+        if guess == numero:
+            print(f"Complimenti! Hai indovinato il numero in {i} tentativi!")
+            break
+        elif guess > numero:
+            vite -= 1
+            i += 1
+            print("Accidenti, hai inserito un numero troppo alto! ")
+            if vite != 0:
+                print(f"Ti rimangono solamente {vite} tentativi!")
+        else:
+            vite -= 1
+            i += 1
+            print("Accidenti, hai inserito un numero troppo basso! ")
+            if vite != 0:
+                print(f"Ti rimangono solamente {vite} tentativi!")
+
+    return()
+
+def main():
+    while True:
+        numero = crea_numero()
+        tentativi = difficolta()
+        confronta(numero, tentativi)
+
+        play = input("Vuoi giocare ancora? Schiaccia 'SI' in caso affermativo, qualsiasi altra cosa altrimenti: ")
+        if play != "SI":
+            print("Grazie per aver giocato con noi! ")
+            break
+
+if __name__ == "__main__":
+    main()
+
